fbc30daa2d3cfd2eb6fd4a809fd60afefcbc34af
KotrynaPapartyna <kmarkeviciute@gmail.com>
2021-08-16 21:11:50 +0300

    Initial commit

---
+++ clients.php
@@ -0,0 +1,34 @@
+<?php 
+    require_once("linkai.php"); 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clients</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+if (!isset($_COOKIE["prisijungta"])) { 
+    header ("Location:index.php"); 
+} else {
+    echo "Sveikas prisijunges prie savo paskyros"; 
+    echo "<form action='clients.php' method='get'>"; 
+    echo "<button class='btn btn-primary' type='submit' name='logout'>"; 
+    echo "</form>"; 
+
+    if (isset($_GET["logout"])) {
+        setcookie("logout", "", time() -3600, "/" ); 
+        header("Location:index.php"); 
+    }
+}
+?>
---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$duomenu_bazes_serveris = "localhost";
+$duomenu_bazes_slapyvardis = "root";
+$duomenu_bazes_slaptazodis = "";
+$duomenu_bazes_pavadinimas = "klientu_valdymo_sistema";
+
+
+$conn = mysqli_connect($duomenu_bazes_serveris,$duomenu_bazes_slapyvardis,$duomenu_bazes_slaptazodis,$duomenu_bazes_pavadinimas);
+
+if(!$conn) {
+    die("Nepavyko prisijungti prie duomenu bazes". mysqli_connect_error());
+
+//} else {
+    //echo "prisijungimas ivyko sekmingai"; 
+}
+
+?>
